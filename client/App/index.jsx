eeade5196a2fa3bd6ab199ce2bc5b2deb0647bb2
Den Temple <developer@dentemple.com>
2018-10-23 15:18:00 -0400

    Converted to stylesheet

--- client/App/index.jsx
+++ client/App/index.jsx
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react'
 
 import Routes from './Routes'
 import Navbar from '../components/features/Navbar'
+import Filterbar from '../components/features/Filterbar'
 
 const App = () => (
   <Fragment>
     <Navbar />
+    <Filterbar />
     <Routes />
   </Fragment>
 )
