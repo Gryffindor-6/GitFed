061ea95131aab119b803b4fd79774283f513c36b
AaronAddison <abartaddison12@gmail.com>
2018-10-24 11:40:22 -0400

    add test button for OAuth

--- client/components/screens/Signup/index.jsx
+++ client/components/screens/Signup/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Signup = () => (
   <div>
     <p>Signup</p>
+    <button><a href='https://github.com/login/oauth/authorize?client_id=7371510084e749ed8e9c&redirect_uri=http://localhost:3000/auth/github'>Github Login</a></button>
   </div>
 )
 
