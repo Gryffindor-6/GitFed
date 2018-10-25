474527c28e026f9b9d8ff4e3cde0d1e0eda6258a
AaronAddison <abartaddison12@gmail.com>
2018-10-25 10:40:23 -0400

    adjust handleClick function

--- client/components/screens/Login/GithubButton.jsx
+++ client/components/screens/Login/GithubButton.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-<<<<<<< HEAD
-const GithubButton = () => (
-  <button><a href='https://github.com/login/oauth/authorize?client_id=7371510084e749ed8e9c&redirect_uri=http://localhost:3000/auth/github'>Github Login</a></button>
-=======
 import Github from '../../common/icons/Github'
 
 const GithubButton = ({ handleClick }) => (
@@ -12,7 +8,6 @@ const GithubButton = ({ handleClick }) => (
     <Github />
     <span>Sign in with Github</span>
   </button>
->>>>>>> a996e7a28ab1d83315ce5107851607416c663800
 )
 
 GithubButton.propTypes = {
