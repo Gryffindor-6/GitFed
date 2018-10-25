5fd94b465b66a29dcaa9b48d7b94bdcf15290188
AaronAddison <abartaddison12@gmail.com>
2018-10-25 10:07:52 -0400

    add middleware to get Github data

--- client/components/screens/Login/GithubButton.jsx
+++ client/components/screens/Login/GithubButton.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const GithubButton = () => (
-  <input
-    type="button"
-    className="loginform__github"
-    value="Sign in with Github"
-  />
+  <button><a href='https://github.com/login/oauth/authorize?client_id=7371510084e749ed8e9c&redirect_uri=http://localhost:3000/auth/github'>Github Login</a></button>
 )
 
 export default GithubButton
--- server/users/routes.js
+++ server/users/routes.js
@@ -7,4 +7,8 @@ router.get('/ping', (_, res) =>
   res.send({ ping: 'ok', route: '/api/users/ping' })
 );
 
+router.get('/data', (req, res, next) => {
+  console.log('data', res.locals.userData);
+});
+
 module.exports = router;
